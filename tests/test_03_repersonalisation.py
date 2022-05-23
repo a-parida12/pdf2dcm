38e78418bfc576f6ba2bfc1bbb4d45a28825bca3
Abhijeet Parida <abhijeet.parida@tum.de>
2022-05-23 18:14:48 -0400

    fix(repersonalisation): non ui field

--- tests/test_03_repersonalisation.py
+++ tests/test_03_repersonalisation.py
@@ -11,9 +11,7 @@ def test_03_1_full_personlisation(pdfencapsconverter):
 
     # with personalisation
     stored_path = pdfencapsconverter.run(path_pdf, ref_dicom)
-    import pdb
 
-    pdb.set_trace()
     assert os.path.exists(stored_path)
     assert pdfencapsconverter.check_valid_dcm(stored_path)
 
@@ -45,7 +43,31 @@ def test_03_2_uid_missing(pdfencapsconverter):
     # check repersonaliation attribute
     assert len(dcm_ds.EncapsulatedDocument) == 898332
     assert dcm_ds.PatientName == ref_dcm_ds.PatientName
-    assert dcm_ds.PatientSex == ref_dcm_ds.PatientSex
+
+    # check the randomly generated uid
     assert dcm_ds.SeriesInstanceUID[:6] == "1.9.9."
+
+    os.remove(stored_path)
+
+
+@pytest.mark.reperson
+def test_03_2_name_missing(pdfencapsconverter):
+    path_pdf = "tests/test_data/test_file.pdf"
+    ref_dicom = "tests/test_data/CT_small_no_name.dcm"
+
+    # with personalisation
+    stored_path = pdfencapsconverter.run(path_pdf, ref_dicom)
+    assert os.path.exists(stored_path)
+    assert pdfencapsconverter.check_valid_dcm(stored_path)
+
+    dcm_ds = pydicom.dcmread(stored_path)
+    ref_dcm_ds = pydicom.dcmread(ref_dicom)
+
+    # check repersonaliation attribute
+    assert len(dcm_ds.EncapsulatedDocument) == 898332
+    assert dcm_ds.SeriesInstanceUID == ref_dcm_ds.SeriesInstanceUID
+
+    # check the empty field
+    assert dcm_ds.PatientName == ""
 
     os.remove(stored_path)
